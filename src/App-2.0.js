82d5548d96abc6b7c8fe6029b942e342cc8a6f43
Federico-I <threxos.48@gmail.com>
2024-08-13 23:39:15 +0100

    renderOptimization-prop-children

--- src/App-2.0.js
+++ src/App-2.0.js
@@ -132,14 +132,17 @@ function List() {
   const { posts } = usePost();
 
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+            <li key={i}>
+              <h3>{post.title}</h3>
+              <p>{post.body}</p>
+            </li>
+        ))}
+      </ul>
+      <Test />
+    </>
   );
 }
 
